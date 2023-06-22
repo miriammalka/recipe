7fef1be49bf6ff60a08e9c5f63d852bcb41b828d
Ashira Friedman <77624864+AshiraFriedman@users.noreply.github.com>
2023-06-22 22:46:46 +0300

    sproc comment

--- Database Files/Sprocs/RecipeDelete.sql
+++ Database Files/Sprocs/RecipeDelete.sql
@@ -2,6 +2,7 @@ create or alter procedure dbo.RecipeDelete(@RecipeId int)
 as
 begin
 	begin try
+	--AF Formatting tip - it's good to indent all the code in the 'try' block, so that it's clear what is part of the 'try' block
 	begin tran
 		delete recipeingredient where RecipeId = @RecipeId
 		delete instruction where RecipeId = @RecipeId
